189fec088911e9f9b6b74650e0b8fba22907cde9
rahmadila1038 <48636971+rahmadila1038@users.noreply.github.com>
2019-04-03 19:31:00 +0700

    nomor 3 & 4 ya, lanjutt

--- Tugas-Node.cpp
+++ Tugas-Node.cpp
@@ -7,6 +7,64 @@ Nama Kelompok:
 */
 #include <iostream>
 using namespace std;
+struct Sllc{
+	int nilai;
+	Sllc *next;
+};
+Sllc *head;
+void init(){
+	head=NULL;
+}
+int isEmpty(){
+	if(head==NULL) return 1;
+	else return 0;
+}
+
+//3.hapus node depan
+void hapusdepan(){
+	Sllc *hapus,*bantu;
+	if(isEmpty()==0){
+		int a;
+		hapus=head;
+		a=head->nilai;
+		if(head->next!=head){
+			bantu=head;
+			while(bantu->next!=head){
+			bantu=bantu->next;
+		}
+		head=head->next;
+		delete hapus;
+		bantu->next=head;
+	}else{
+		head=NULL;
+	}
+	cout<<"nilai terhapus\n";
+	
+}else cout<<"nilai kosong\n";
+}
+//4.hapus node belakang
+void hapusbelakang(){
+	Sllc *hapus,*bantu;
+	if(isEmpty()==0){
+		int a;
+		hapus=head;
+		if(head->next==head){
+			head=NULL;
+		}else{
+			bantu=head;
+			while(bantu->next->next!=head){
+				bantu=bantu->next;
+			}
+			hapus=bantu->next;
+			a=bantu->nilai;
+			bantu->next=head;
+			delete hapus;
+		}
+		cout<<"nilai terhapus\n";
+	}else cout<<"nilai kosong\n";
+	
+}
+
 int main ()
 {
 
