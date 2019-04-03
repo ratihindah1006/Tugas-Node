08d666b5bac3a4b8d15753636ce13e11d142b5a5
ratihindah1006 <ratihindah514@gmail.com>
2019-04-03 17:51:25 +0700

    Mulai Dari Sini Ya

---
+++ README.md
@@ -0,0 +1,1 @@
+Repo ini untuk Tugas Node
---
+++ Tugas-Node.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+/*
+Nama Kelompok:
+1. Ratih Indah Wardani	1817051006
+2. Intania Rahmadhilla	1817051025
+3. Rahmadila Nurjannah	1817051038
+4. Dewi Lestari		1817051041
+*/
+}
