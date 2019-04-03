06cf0898d6d709f7bf71ef428e5f1f83a0275118
rahmadila1038 <48636971+rahmadila1038@users.noreply.github.com>
2019-04-04 06:04:55 +0700

    finish

--- Tugas-Node.cpp
+++ Tugas-Node.cpp
@@ -161,16 +161,18 @@ void cetaknilai(){
 	
 }
 	void tanya(){
-	int pilihan,nilaibaru;
+	int pilihan,nilaibaru,nilai,letak;
 	cout<<"**********************************************\n";
 	cout<<" \t\tsilahkan pilih?\n";
 	cout<<"**********************************************\n";
 	cout<<"1.tambah nilai di depan\n";
-	cout<<"2.belakang\n";
-	cout<<"3.hapus dpan\n";
-	cout<<"4.hapus belakang\n";
-	cout<<"5.tambah tengah\n";
-	cout<<"6.hapus tengah\n";
+	cout<<"2.tambah nilai di belakang\n";
+	cout<<"3.hapus nilai depan \n";
+	cout<<"4.hapus nilai belakang\n";
+	cout<<"5.tambah nilai di posisi tertentu\n";
+	cout<<"6.hapus nilai di posisi tertentu\n";
+	cout<<"7.tampilkan semua nilai\n";
+	cout<<"8.keluar\n";
 	cout<<"masukkan pilihan anda \n";
 	cin>>pilihan;
 	switch(pilihan){
@@ -178,31 +180,37 @@ void cetaknilai(){
 			cout<<"masukkan nilai ";
 			cin>>nilaibaru;
 			tambahdepan( nilaibaru);
-			cetaknilai();
 			break;
 		case 2:
 			cout<<"masukkan nilai ";
 			cin>>nilaibaru;
 			tambahbelakang( nilaibaru);
-			cetaknilai();
 			break;
 		case 3:
 			hapusdepan();
-			cetaknilai();
 			break;
 		case 4:
 			hapusbelakang();
-			cetaknilai();
 			break;
 		case 5:
+			cout<<"tambah di posisi ke- ?";
+			cin>>letak;
 			cout<<"masukkan nilai ";
-			cin>>nilaibaru;
+			cin>>nilai;
 			tambahtengah(1,3);
+			break;
+		case 6:
+			cout<<"hapus node di posisi ke- ?";
+			hapustengah(nilai);
+			break;
+		case 7 : 
+			sysstem("cls")
 			cetaknilai();
 			break;
+		case 8 :
+			cin.get();
 		default:
 			cout<<"kode salah\n";
-			system("pause");
 			system("cls");
 	}}
 	
@@ -218,7 +226,6 @@ int main ()
 		tanya();
 	
 	}
-	system("cls");
 ;
 	return 0;
 }
